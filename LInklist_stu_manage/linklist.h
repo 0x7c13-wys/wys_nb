c5e936b83cd89d02949a80a573507b0f1e629fe5
王玉升 <wys031012@foxmail.com>
2023-05-22 12:11:42 +0800

    增加了链表学生信息管理系统

---
+++ LInklist_stu_manage/linklist.h
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<iomanip>
+using namespace std;
+#define OK 1
+#define ERROR 0
+#define OVERFLOW -2
+typedef int Status; //Status 是函数返回值类型，其值是函数结果状态代码。
