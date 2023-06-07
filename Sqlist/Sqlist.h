1fe696e2d6e3bef1ca04570914a6daf9291c0f50
王玉升 <wys031012@foxmail.com>
2023-06-07 20:55:16 +0800

    增加了ADT的头文件

--- Sqlist/Sqlist.h
+++ Sqlist/Sqlist.h
@@ -1,3 +1,6 @@
+/* 
+
+
 #define MAX_STUDENG 500;
 
 #include<iostream>
@@ -46,15 +49,18 @@ voiud Init_list(Sqlist &S){
     S.elem = new information[MAX_STUDENG];
     S.length = 0;
 }
+ */
 
 
 
 
 
-/*
+/* 
+
+
 输入学生信息
 @S:传入顺序表，通过顺序表读取并修改学生信息
-*/
+
 void infromation_Input(Sqlist &S){
     char choose = 'A';
     for(int i = 0; i < S.length; i++){
@@ -85,3 +91,105 @@ void infromation_Input(Sqlist &S){
     }
 }
 
+
+*/
+
+
+#define MAX_SIZE 1000
+
+#define OK 1
+#define ERROR 0   
+#define OVERFLOW -2
+#include<iostream>
+using namespace std;
+
+typedef int Status; //Status 是函数返回值类型，其值是函数结果状态代码。
+
+/*
+创建数据类型
+*/
+typedef struct{
+    char name[10];                          //名字
+    int age;                                //年龄
+}Stu;
+
+/*
+创建顺序表
+*/
+typedef struct{
+    int length;                     //顺序表长度
+    Stu *elem;                      //顺序表元素
+}Sqlist;
+
+Sqlist L;
+
+
+/*
+初始化顺序表
+*/
+Status Init_List(Sqlist &L){
+    L.elem = new Stu[MAX_SIZE];             //为顺序表分配空间
+    if(!L.elem){
+        exit(OVERFLOW);                     //分配失败
+    }
+    L.length = 0;                           //初始化顺序表长度
+    return OK;
+}
+
+
+/*
+顺序表的取值
+*/
+Status GetElem(Sqlist L, int i, Stu &e){
+    if(i < 1 || i > L.length){                  //判断i的范围是否有效
+        return ERROR;               
+    }
+    e = L.elem[i - 1];
+    return OK;
+}
+
+
+
+/*
+顺序表的查找
+*/
+Status LocateElem(Sqlist L, Stu e){
+    for(int i = 0; i < L.length; i++){
+        if(L.elem[i].age == e.age){
+            return i + 1;
+        }
+    }
+    return ERROR;
+}
+
+/*
+顺序表的插入
+*/
+Status ListInsert(Sqlist &L, int i, Stu e){
+    if(i < 1 || i > L.length + 1){                  //判断i的范围是否有效
+        return ERROR;               
+    }
+    if(L.length == MAX_SIZE){                       //判断顺序表是否已满
+        return ERROR;
+    }
+    for(int j = L.length - 1; j >= i - 1; j--){     //将第i个元素及之后的元素后移
+        L.elem[j + 1] = L.elem[j];
+    }
+    L.elem[i - 1] = e;                              //将e插入到第i个位置
+    L.length++;                                     //顺序表长度加1
+    return OK;
+}
+
+/*
+顺序表的删除
+*/
+Status ListDelete(Sqlist &L, int i){
+    if(i < 1 || i > L.length){                  //判断i的范围是否有效
+        return ERROR;               
+    }
+    for(int j = i; j < L.length; j++){          //将第i个元素之后的元素前移
+        L.elem[j - 1] = L.elem[j];
+    }
+    L.length--;                                 //顺序表长度减1
+    return OK;
+}
