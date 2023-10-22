36d3986144001963ea8ca162e1d1da7d7ba70a5e
王玉升 <wys031012@foxmail.com>
2023-10-22 22:16:56 +0800

    Java实验三

---
+++ Java/test/sy3/src/main6.java
@@ -0,0 +1,17 @@
+public class main {
+    public static void main(String[] args) {
+
+        CPU cpu = new CPU();
+        cpu.setSpeed(2200);
+
+        HardDisk disk = new HardDisk();
+        disk.setAmount(500);
+
+
+        PC pc = new PC();
+        pc.Cpu = cpu;
+        pc.HD = disk;
+
+        pc.show();
+    }
+}
